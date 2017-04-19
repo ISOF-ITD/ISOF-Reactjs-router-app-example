df30de51391eca96b85f186254993c055e853ee6
traustid <traustid@gmail.com>
2017-04-19 09:02:51 +0200

    Lagt till kommentarer i ExampleFetchModule.js

--- scripts/components/ExampleFetchModule.js
+++ scripts/components/ExampleFetchModule.js
@@ -14,10 +14,12 @@ export default class ExampleFetchModule extends React.Component {
 		};
 	}
 
+	// Vi anroper getData när vår modul finns på webbsidan
 	componentDidMount() {
 		this.getData(this.props.params.category || null);
 	}
 
+	// Vi anroper getData när vår modul tar emor nya parameters
 	componentWillReceiveProps(props) {
 		this.getData(props.params.category || null)
 	}
@@ -25,8 +27,10 @@ export default class ExampleFetchModule extends React.Component {
 	getData(category) {
 		fetch('http://www4.sprakochfolkminnen.se/sagner/api/records/0/50/type/tryckt;arkiv/'+(category ? 'category/'+category : ''))
 			.then(function(response) {
+				// Första steget i fetch är att leverera data som json till nästa steg
 				return response.json()
 			}).then(function(json) {
+				// Nu kan vi använda den data som vi hämtat, vi lägger det inn i state objectet
 				this.setState({
 					data: json.data
 				});
@@ -37,6 +41,9 @@ export default class ExampleFetchModule extends React.Component {
 	}
 
 	render() {
+		// Uppdaterar webbsidan med ny data från state objektet
+
+		// Skapar en lista över alla sägner som hittats
 		var recordsData = this.state.data.length > 0 ? this.state.data.map(function(item, index) {
 			return <div key={index}><a href={'http://www.sprakochfolkminnen.se/test-och-utveckling/kartor/sagenkarta.html#/record/'+item.id}>{item.title}</a></div>;
 		}) : [];
